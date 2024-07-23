fb1c917860b2d4c0415e31830998857ce0a069a9
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-07-23 09:55:18 +0200

    Add service discovery for Alfen chargers

--- drivers/ocpp/driver.ts
+++ drivers/ocpp/driver.ts
@@ -1,10 +1,28 @@
-import { Driver } from 'homey'
+import { DiscoveryResultMDNSSD, Driver } from 'homey'
 import { PairSession } from 'homey/lib/Driver'
 
 class OCPPDriver extends Driver {
 
-    async onPair(sessoin: PairSession) {
+    async onPair(session: PairSession) {
+        const driver = this
 
+        session.setHandler('list_devices', async function () {
+            const discoveryStrategy = driver.getDiscoveryStrategy()
+            const discoveryResults = discoveryStrategy.getDiscoveryResults() as { [key: string]: DiscoveryResultMDNSSD }
+            return Object.values(discoveryResults).map((result: DiscoveryResultMDNSSD) => {
+                return {
+                    name: result.name,
+                    data: {
+                        id: result.name,
+                    },
+                    store: {
+                        address: result.address,
+                        host: result.host,
+                        port: result.port,
+                    }
+                }
+            })
+        })
     }
 }
 
